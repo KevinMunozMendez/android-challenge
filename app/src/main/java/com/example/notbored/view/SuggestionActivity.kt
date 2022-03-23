c6eecc580c24bc9a9597c74e5f3c2e5c643029c9
yediaz <yesid.diaz@mercadolibre.com.co>
2022-03-23 16:04:56 -0500

    Add random activity, add back button into the toolbar and add function try another.

--- app/src/main/java/com/example/notbored/view/SuggestionActivity.kt
+++ app/src/main/java/com/example/notbored/view/SuggestionActivity.kt
@@ -1,21 +1,17 @@
 package com.example.notbored.view
 
-import android.app.Activity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.notbored.common.ActivitiesService
 import com.example.notbored.common.entities.ActivityEntity
 import com.example.notbored.databinding.ActivitySuggestionBinding
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.function.LongFunction
 
 
 class SuggestionActivity : AppCompatActivity() {
@@ -28,6 +24,14 @@ class SuggestionActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupViewModel()
+
+        binding.topAppBar.setNavigationOnClickListener {
+            finish()
+        }
+
+        binding.btnTryAnother.setOnClickListener {
+            setupViewModel()
+        }
     }
 
     private fun setupViewModel() {
@@ -36,36 +40,45 @@ class SuggestionActivity : AppCompatActivity() {
         val type = objectIntent.getStringExtra("type")
 
         val endpoint = "activity?type=$type&participants=$participants"
+        val endpoint2 = "activity?participants=$participants"
 
-        lifecycleScope.launch {
-            setUi(getContacts(endpoint))
+        if (!type.isNullOrBlank()) {
+            binding.topAppBar.title = type.replaceFirstChar { it.uppercase() }
+            binding.layoutType.visibility = View.GONE
+            lifecycleScope.launch {
+                setUi(getContacts(endpoint))
+            }
+        } else {
+            binding.topAppBar.title = "Random"
+            binding.layoutType.visibility = View.VISIBLE
+            lifecycleScope.launch {
+                setUi(getContacts(endpoint2))
+            }
         }
     }
 
-    private suspend fun getContacts(endpoint: String): ActivityEntity = withContext(Dispatchers.IO) {
-
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://www.boredapi.com/api/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val service: ActivitiesService = retrofit.create(ActivitiesService::class.java)
-        service.getContacts(endpoint)
-    }
+    private suspend fun getContacts(endpoint: String): ActivityEntity =
+        withContext(Dispatchers.IO) {
+            val retrofit: Retrofit = Retrofit.Builder()
+                .baseUrl("https://www.boredapi.com/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            val service: ActivitiesService = retrofit.create(ActivitiesService::class.java)
+            service.getContacts(endpoint)
+        }
 
     private fun setUi(activity: ActivityEntity) {
 
         val price = (activity.price) * 10
-        when(price.toInt()) {
+        when (price.toInt()) {
             0 -> binding.tvPrice.text = "Free"
             in 1..3 -> binding.tvPrice.text = "Low"
             in 4..6 -> binding.tvPrice.text = "Medium"
             else -> binding.tvPrice.text = "High"
-
         }
 
         binding.title.text = activity.activity
         binding.numParticipants.text = activity.participants.toString()
+        binding.tvActivity.text = activity.type.replaceFirstChar { it.uppercase() }
     }
-
 }
